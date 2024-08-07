5322355e6d88f5e0b8dfed73272ee3074db9b6c2
zbornak <zbornak@protonmail.com>
2024-08-07 09:48:40 +0100

    Initial Commit

---
+++ CDArkive2/CDArkive2App.swift
@@ -0,0 +1,17 @@
+//
+//  CDArkive2App.swift
+//  CDArkive2
+//
+//  Created by Mark Strijdom on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CDArkive2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
